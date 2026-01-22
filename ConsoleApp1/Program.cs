6903749edd8f83bfb291968b30ce3235d0f2d45e
SHRAVANI <SHRAVANI@LAPTOP-UUQNILDI>
2026-01-22 09:35:59 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        void Display()
+        {
+            Console.WriteLine("Welcome to pune");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
